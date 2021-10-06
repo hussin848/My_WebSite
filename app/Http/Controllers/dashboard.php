ec90dfb727b14a3c9466705809175fc93dff889f
Your Name <lufe9058@gmail.com>
2021-10-06 05:09:19 +0300

    auth

--- app/Http/Controllers/dashboard.php
+++ app/Http/Controllers/dashboard.php
@@ -7,11 +7,7 @@ use Illuminate\Http\Request;
 
 class dashboard extends Controller
 {
-    public function __construct()
-    {
 
-        $this->middleware('auth');
-    }
 
 
     public function index()
